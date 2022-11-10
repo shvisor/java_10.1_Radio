dc85427a38f4a041d65271a005b14c3e1d617300
Max Shvagerus <mshvagerus@gmail.com>
2022-11-10 17:39:15 +0300

    Lombok Tests

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
 
     @Test
-    public void shouldDefaultNumber() {
+    public void shouldDefaultNumber() { // передача данных через конструктор
         Radio radio = new Radio();
 
         int expected = 10;
@@ -16,7 +16,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldNextNumber() {
+    public void shouldNextNumber() { // передача данных через конструктор
         Radio radio = new Radio(6, 8, 30, 0, 29, 0, 100);
 
         radio.nextNumber();
@@ -28,7 +28,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldNextNumberAfterMax() {
+    public void shouldNextNumberAfterMax() { // передача данных через конструктор
         Radio radio = new Radio(29, 8, 30, 0, 29, 0, 100);
 
         radio.nextNumber();
@@ -40,7 +40,22 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldPreviousNumber() {
+    public void shouldNextNumberAfterMaxWithSetter() { // передача данных через сеттеры
+        Radio radio = new Radio();
+
+        radio.setNumber(30);
+        radio.setCurrentNumber(29);
+        radio.setMaxNumber(29);
+        radio.nextNumber();
+
+        int expected = 0;
+        int actual = radio.getCurrentNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPreviousNumber() { // передача данных через конструктор
         Radio radio = new Radio(5, 8, 30, 0, 29, 0, 100);
 
         radio.prevNumber();
@@ -52,7 +67,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldPreviousNumberBeforeMin() {
+    public void shouldPreviousNumberBeforeMin() { // передача данных через конструктор
         Radio radio = new Radio(0, 8, 30, 0, 29, 0, 100);
 
         radio.prevNumber();
@@ -62,9 +77,24 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
-//
+
     @Test
-    public void shouldNumberAboveMax() {
+    public void shouldPreviousNumberBeforeMinWithSetter() { // передача данных через сеттеры
+        Radio radio = new Radio(0, 8, 30, 0, 29, 0, 100);
+
+        radio.setNumber(30);
+        radio.setCurrentNumber(0);
+        radio.setMinNumber(0);
+        radio.prevNumber();
+
+        int expected = 29;
+        int actual = radio.getCurrentNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNumberAboveMax() { // передача данных через конструктор
         Radio radio = new Radio(31, 8, 30, 0, 29, 0, 100);
 
         radio.nextNumber();
@@ -76,7 +106,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldNumberBelowMin() {
+    public void shouldNumberBelowMin() { // передача данных через конструктор
         Radio radio = new Radio(-2, 8, 30, 0, 29, 0, 100);
 
         radio.prevNumber();
@@ -88,7 +118,7 @@ public class RadioTest {
     }
 
    @Test
-   public void shouldSpecificallyNumber () {
+   public void shouldSpecificallyNumber () { // передача данных через конструктор
        Radio radio = new Radio(3, 8, 30, 0, 29, 0, 100);
 
        int expected = 3;
@@ -98,7 +128,7 @@ public class RadioTest {
    }
 
     @Test
-    public void shouldIncreaseVolume() {
+    public void shouldIncreaseVolume() { // передача данных через конструктор
         Radio radio = new Radio(3, 8, 30, 0, 29, 0, 100);
 
         radio.increaseVolume();
@@ -110,7 +140,21 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldDecreaseVolume() {
+    public void shouldIncreaseVolumeWithSetter() { // передача данных через сететры
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(8);
+        radio.setMaxVolume(100);
+        radio.increaseVolume();
+
+        int expected = 9;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseVolume() { // передача данных через конструктор
         Radio radio = new Radio(3, 8, 30, 0, 29, 0, 100);
 
         radio.decreaseVolume();
@@ -122,7 +166,21 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldIncreaseVolumeMax() {
+    public void shouldDecreaseVolumeWithSetter() { // передача данных через сететры
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(8);
+        radio.setMinVolume(0);
+        radio.decreaseVolume();
+
+        int expected = 7;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolumeMax() { // передача данных через конструктор
         Radio radio = new Radio(3, 100, 30, 0, 29, 0, 100);
 
         radio.increaseVolume();
@@ -134,7 +192,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldDecreaseVolumeMin() {
+    public void shouldDecreaseVolumeMin() { // передача данных через конструктор
        Radio radio = new Radio(3, 0, 30, 0, 29, 0, 100);
 
         radio.decreaseVolume();
@@ -146,7 +204,7 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldIncreaseVolumeAboveMax() {
+    public void shouldIncreaseVolumeAboveMax() { // передача данных через конструктор
         Radio radio = new Radio(3, 102, 30, 0, 29, 0, 100);
 
         radio.increaseVolume();
@@ -158,13 +216,23 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldDecreaseVolumeBelowMin() {
+    public void shouldDecreaseVolumeBelowMin() { // передача данных через конструктор
         Radio radio = new Radio(3, -3, 30, 0, 29, 0, 100);
 
         radio.decreaseVolume();
 
         int expected = 0;
         int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldHashCode() {
+        Radio radio = new Radio(3, 15, 30, 0, 29, 0, 100);
+
+        int expected = 2055554250;
+        int actual = radio.hashCode();
 
         Assertions.assertEquals(expected, actual);
     }
